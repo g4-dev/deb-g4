3f5467f8cdba12532f45db2e6656e5a6616a16be
loic-roux <69loloro10@gmail.com>
2020-02-17 22:10:45 +0100

    cleaner inspired from bento

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,10 +1,60 @@
 #!/bin/bash -eux
 
+# Delete all Linux headers
+dpkg --list \
+  | awk '{ print $2 }' \
+  | grep 'linux-headers' \
+  | xargs apt-get -y purge;
+
+# Remove specific Linux kernels, such as linux-image-3.11.0-15 but
+# keeps the current kernel and does not touch the virtual packages,
+# e.g. 'linux-image-amd64', etc.
+dpkg --list \
+    | awk '{ print $2 }' \
+    | grep 'linux-image-[234].*' \
+    | grep -v `uname -r` \
+    | xargs apt-get -y purge;
+
+# Delete Linux source
+dpkg --list \
+    | awk '{ print $2 }' \
+    | grep linux-source \
+    | xargs apt-get -y purge;
+
+# Delete development packages
+dpkg --list \
+    | awk '{ print $2 }' \
+    | grep -- '-dev$' \
+    | xargs apt-get -y purge;
+
+# Delete X11 libraries
+apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6;
+apt-get -y purge gcc g++;
+
+# Delete obsolete networking
+apt-get -y purge ppp pppconfig pppoeconf;
+
+# Delete oddities
+apt-get -y purge popularity-contest;
+apt-get -y purge installation-report;
+
+# truncate any logs that have built up during the install
+find /var/log -type f -exec truncate --size=0 {} \;
+
+# Blank netplan machine-id (DUID) so machines get unique ID generated on boot.
+truncate -s 0 /etc/machine-id
+
+# clear the history so our install isn't there
+export HISTSIZE=0
+rm -f /root/.wget-hsts
+
 # Uninstall Ansible and dependencies.
 pip uninstall ansible
-apt-get remove python-pip python-dev
+apt-get -y purge python-pip python-dev python python2.7 python3;
 
 # Apt cleanup.
+apt-get -y autoremove;
+apt-get -y clean;
 apt autoremove
 apt update
 
@@ -14,6 +64,9 @@ rm -f /home/vagrant/*.sh
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
+
+sudo dpkg --purge --force-all installation-report || true
+sudo journalctl --vacuum-size=100M -q
 
 # Add `sync` so Packer doesn't quit too early, before the large file is deleted.
 sync
