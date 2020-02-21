2fda67a4159545e4432b9e2b0a8fd13194fb8e5d
loic-roux <69loloro10@gmail.com>
2020-02-21 18:53:14 +0100

    new vbox

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -20,7 +20,6 @@ cat <<EOF >/etc/apt/apt.conf.d/10periodic;
 APT::Periodic::Enable "0";
 APT::Periodic::Update-Package-Lists "0";
 APT::Periodic::Download-Upgradeable-Packages "0";
-APT::Periodic::AutocleanInterval "0";
 APT::Periodic::Unattended-Upgrade "0";
 EOF
 
